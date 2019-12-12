e97d912ba97459da0225d053b04b27bd1decbfbb
Junior Reis <33906887+reisjunior1@users.noreply.github.com>
2019-12-12 18:37:20 -0300

    perfil atualização 1

--- Laravel/Trabalho_Final/resources/views/admin/login/index.blade.php
+++ Laravel/Trabalho_Final/resources/views/admin/login/index.blade.php
@@ -3,15 +3,17 @@
 @section('content')
 
     <div class="container">
-        <h2>Entrar</h2>
-        <form action="{{route('admin.login')}}" method="POST">
-            {{csrf_field()}}
-            @include('admin.login._form')
-            <button class="btn blue">Entrar</button>
-        </form>
-        <br><br>
-        <a href="{{route('admin.cadastrar')}}">Cadastra-se</a>
+        <div class="row">
+            <h2>Entrar</h2>
+            <form action="{{route('admin.login')}}" method="POST">
+                {{csrf_field()}}
+                @include('admin.login._form')
+                <button class="btn blue">Entrar</button>
+            </form>
+            <br><br>
+            <a href="{{route('admin.cadastrar')}}">Cadastra-se</a>
+        </div>
+        
     </div>
-    <br><br>
 
 @endsection
--- Laravel/Trabalho_Final/resources/views/admin/perfil.blade.php
+++ Laravel/Trabalho_Final/resources/views/admin/perfil.blade.php
@@ -1,1 +1,15 @@
-<h1>Bem-vindo ao seu perfil</h1>
+@extends('layouts.site')
+
+@section('content')
+
+    <div class="container">
+        <div class="row">
+            <h3>Bem-vindo ao seu perfil</h3>
+            <button type="submit"class="btn blue"> 
+                 <a class="grey-text text-lighten-3" href="{{route('admin.cadastrar-animal')}}">Colocar Animal para Adoção</a>
+            </button>
+        </div>
+        <br><br>
+    </div>
+
+@endsection
