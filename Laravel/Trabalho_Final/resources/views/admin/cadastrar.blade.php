86352e96371ec8d3dbaf2ad53d75363268f7e1a8
Junior Reis <33906887+reisjunior1@users.noreply.github.com>
2019-12-12 10:30:58 -0300

    view cadastro atualização 1

--- Laravel/Trabalho_Final/resources/views/admin/cadastrar.blade.php
+++ Laravel/Trabalho_Final/resources/views/admin/cadastrar.blade.php
@@ -2,21 +2,24 @@
 
 @section('content')
 <div class="container">
-    <form action="" method="POST" class = "form-field">
-        {{csrf_field()}}
-        <div class="input-field">
-            <input type="text" name="name" class="validate" >
-            <label>Nome:<label> 
-        </div>
-        <div class="input-field">
-            <input type="text" name="email" class="validate" >
-            <label>email:<label> 
-        </div>
-        <div class="input-field">
-            <input type="password" name="password" class="validate" >
-            <label>Senha:<label> 
-        </div>
-        <button type="submit"class="btn blue">Cadastrar</button>
-    </form>
+    <h2 class="center">Cadastrar</h2>
+    <div class="row">
+        <form action="{{route('admin.salvar')}}" method="POST" class = "form-field">
+            {{csrf_field()}}
+            <div class="input-field">
+                <input type="text" name="name" class="validate" >
+                <label>Nome:<label> 
+            </div>
+            <div class="input-field">
+                <input type="text" name="email" class="validate" >
+                <label>email:<label> 
+            </div>
+            <div class="input-field">
+                <input type="password" name="password" class="validate" >
+                <label>Senha:<label> 
+            </div>
+            <button type="submit"class="btn blue">Cadastrar</button>
+        </form>
+    </div>
 </div>
 @endsection
