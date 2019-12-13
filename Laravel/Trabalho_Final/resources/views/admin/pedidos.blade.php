f360076775835bb3e0adce3b181567f519e59e75
Junior Reis <33906887+reisjunior1@users.noreply.github.com>
2019-12-13 11:05:17 -0300

    pedido de adoção e minha adoçoes atualizado
    agora com join

--- Laravel/Trabalho_Final/resources/views/admin/pedidos.blade.php
+++ Laravel/Trabalho_Final/resources/views/admin/pedidos.blade.php
@@ -5,6 +5,7 @@
     <div class="container">
         <div class="row">
         <h3>Pedidos de adoção</h3>
+        <h6>tabela mostra os animais que vc colocou em adoção e que tem pessoas querendo adotar</h6>
         <table>
         <thead>
           <tr>
@@ -18,7 +19,7 @@
             @foreach($animais as $animal)
                 <tr scope="row">
                     <td>{{$animal->id}}</td>
-                    <td>{{$animal->nome}}</td>
+                    <td>{{$animal->pedinte_nome}}</td>
                     colocar o email de quem ta pedindo pra adotar
                 </tr>
             @endforeach 
