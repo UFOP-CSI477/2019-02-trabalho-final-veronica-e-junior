7ec0dfdc6e9c2ceac401a1f5d4f8d5d978f08335
Junior Reis <33906887+reisjunior1@users.noreply.github.com>
2019-12-13 13:00:45 -0300

    Update _filtros.blade.php

--- Laravel/Trabalho_Final/resources/views/layouts/_site/_filtros.blade.php
+++ Laravel/Trabalho_Final/resources/views/layouts/_site/_filtros.blade.php
@@ -1,34 +1,40 @@
 <div class="container">
     <label>Busque pela cidade</label>
-    <form action="{{route('site.busca')}}" method="POST" class="form-inline my-3 my-lg-0">
-      {{ csrf_field() }}
-      <input class="form-control mr-2" style="width: 30%" name="texto" type="text" placeholder="Buscar" aria-label="Search">
-
-         
-                <label>
-                    <input class="especie" name="especie" type="radio" value="cachorro" />
-                    <span>Cães</span>
-                </label>
+    <div class="row">
+        <div class="card  grey lighten-3">
+            <div class="card-content white-text" align="center">
+            <span class="card-title black-text">Faça uma busca</span>
+                <form action="{{route('site.busca')}}" method="POST" class="form-inline my-3 my-lg-0">
+                    {{ csrf_field() }}
+                    <input class="form-control mr-2" style="width: 80%" name="texto" type="text" placeholder="Cidade" aria-label="Search">
+                    <br>
             
-                <label>
-                    <input class="especie" name="especie" type="radio" value="gato"/>
-                    <span>Gatos</span>
-                </label>
-          
+                    <label>
+                        <input class="especie" name="especie" type="radio" value="cachorro" />
+                        <span>Cães</span>
+                    </label>
+                
+                    <label>
+                        <input class="especie" name="especie" type="radio" value="gato"/>
+                        <span>Gatos</span>
+                    </label>
             
-          
-                <label>
-                    <input class="sexo" name="sexo" type="radio" value="femea" />
-                    <span>Fêmeas</span>
-                </label>
-           
-                <label>
-                    <input class="sexo" name="sexo" type="radio" value="macho"/>
-                    <span>Machos</span>
-                </label>
-          
-
-      <button class="btn btn-outline-warning my-2 my-sm-0" type="submit">Filtrar</button>
-    </form>
-  </div>
+                
+            
+                    <label>
+                        <input class="sexo" name="sexo" type="radio" value="femea" />
+                        <span>Fêmeas</span>
+                    </label>
+            
+                    <label>
+                        <input class="sexo" name="sexo" type="radio" value="macho"/>
+                        <span>Machos</span>
+                    </label>
+                    <br><br>
+                    <button class="btn btn-outline-warning my-2 my-sm-0" type="submit">Filtrar</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
   <br><br>
